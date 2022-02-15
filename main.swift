ce7aae09cb5e357269d7fc031ed709afff834d5a
MarioArceo <99693772+MarioArceo@users.noreply.github.com>
2022-02-15 17:19:32 +0000

    Primera Prototipo funcional del mecanismo evolucionado

--- main.swift
+++ main.swift
@@ -1,16 +1,14 @@
 //Se crea el loop para repetir funcionalidad
 var loop:String = "n"
-repeat{
+
   print("Calculadora en Swift")
   print("Bienvenido...")
 
-  //Se extraen los valores de algun lado
-  //var valor1: Int = 0//, valor2: Double = 0.0, opc: String?  
   var accion:String = "", n1:String = "", n2:String = "", operacion:String = "", res:Double = 0.0, rep:Bool = true, operador:String = ""
+  var restemp:String = ""
 
   print("Digite el primer numero")
   n2 = readLine()!
-  //valor1 = Int(n2)!
   res = res + Double(n2)!
   operacion = operacion + n2
 
@@ -19,37 +17,31 @@ repeat{
    switch(accion) {
       case "+" :
           if(n1 == "" ){
-            print ("n1 esta vacio")
+            n1 = n2            
           }else{
-            res = Double(Int(n1)!) + Double(Int(n2)!)
+            res = Double(n1)! + Double(Int(n2)!)
             n1 = String(res)            
-            print ("El Resultado es: ", n1)
-          }
-          
-          rep = true 
-          n1 = n2 
-          n2 = ""
-          operador = accion
-          operacion = operacion + accion
-          print (operacion)
-          print (n2)
+          }          
+          func1()
           break; 
       default : 
           rep = true          
           operacion = operacion + accion
           n2 = n2 + accion
-          print (operacion)
           if(n1 == "" ){
-          }else{         
-            print ("El Resultado es: ", n1)
+
+          }else{     
+            restemp = String(Double(n1)! + Double(Int(n2)!))   
+            print ("El Resultado es: ", restemp) 
           }
     }
-
+  print (operacion)
 }while(rep != false)
 
+func func1(){
+  rep = true   
+  n2 = ""
+  operador = accion
+  operacion = operacion + accion
+}
 
-  print("Desea realizar otra operacion ('s'/'n')")
-  loop = readLine()!
-  print("--------------------------------------------------------")
-
-}while(loop == "s" || loop == "si" || loop == "Si" || loop == "SI")
